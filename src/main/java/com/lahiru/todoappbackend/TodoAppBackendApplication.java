27ef8e8c7de16e1fab667424d60755dc9dd582f0
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-11-10 19:50:36 +1100

    security module

--- src/main/java/com/lahiru/todoappbackend/TodoAppBackendApplication.java
+++ src/main/java/com/lahiru/todoappbackend/TodoAppBackendApplication.java
@@ -18,3 +18,14 @@ public class TodoAppBackendApplication {
     }
 
 }
+
+
+//    Deprecated csrf() method:
+//
+//        http.csrf().disable()
+//        New version of csrf() method:
+//
+//        http.csrf((csrf) -> csrf.disable())
+//        In the next lecture, please use the below new csrf() method that takes lambda expression implementation to disable the CSRF feature:
+//
+//        http.csrf((csrf) -> csrf.disable())
