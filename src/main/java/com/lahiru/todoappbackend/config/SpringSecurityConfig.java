ac135e380ba9d66ee44d91a32cdde60c88b43331
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-11-14 23:46:24 +1100

    added role based security

--- src/main/java/com/lahiru/todoappbackend/config/SpringSecurityConfig.java
+++ src/main/java/com/lahiru/todoappbackend/config/SpringSecurityConfig.java
@@ -2,21 +2,56 @@ package com.lahiru.todoappbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
 
     @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
 
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST, "api/todos").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "api/todos").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "api/todos").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "api/todos").hasAnyRole("ADMIN", "USER");
+                    authorize.requestMatchers(HttpMethod.PATCH, "api/todos").hasAnyRole("ADMIN", "USER");
+//                    authorize.requestMatchers(HttpMethod.GET, "api/todos").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails lahiru = User.builder()
+                .username("Admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails liam = User.builder()
+                .username("Liam")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(lahiru, liam);
     }
 
 }
